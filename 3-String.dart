77973b80dcfc6dab58560cbae60f335c56729e3e
Umer Farooq Jillani <umerfarooqjillani92@gmail.com>
2025-05-29 15:12:19 +0500

    Add the String concatenation

---
+++ 3-String.dart
@@ -0,0 +1,13 @@
+
+void main(){
+  // String Concatenation or Interpolation
+  String str1 = "Umer";
+  String str2 = "Farooq";
+  String str3 = str1 + str2;
+  print("Hey ${str1 +str2}");
+  int num = 2;
+  print("This is a string : " + num.toString());
+  // ---------------------------------------------------------------
+  
+
+}
