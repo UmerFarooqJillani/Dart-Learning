a1447dbdc9d86624c94fa82b7ab32292d50ded84
UmerFarooqJillani <umerfarooqjillai92@gmail.com>
2025-11-11 10:28:53 +0500

    Interview Q/A: Shallow Copy & Deep Copy?

---
+++ Interviews Q_A.dart
@@ -0,0 +1,28 @@
+class Student {
+  String name;
+  List<int> marks;
+
+  Student(this.name, this.marks);
+}
+
+void main() {
+  // --------------- (Shallow Copy & Deep Copy) --------------------------------------
+  /* 
+  -> What is Shallow Copy and Deep Copy?
+    Shallow Copy:
+        - A Shallow Copy copies only the top-level structure of an object.
+        - If the object contains references to other objects (like lists or maps), 
+          the copy shares those references (not duplicates them).
+    Deep Copy:
+        - A Deep Copy creates a fully independent clone, duplicating all nested objects too.
+  */
+  Student s1 = Student('Ali', [85, 90]);
+  Student shallow = s1; // Shallow copy (same reference)
+  Student deep = Student(s1.name, List.from(s1.marks)); // Deep copy
+
+  shallow.marks[0] = 50;
+  print(s1.marks); // [50, 90] – changed (same reference)
+
+  deep.marks[0] = 100;
+  print(s1.marks); // [50, 90] – not affected (separate copy)
+}
